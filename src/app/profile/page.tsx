ed6c592e1643b49315d04aff70b1c670c326f50e
AkshatJha21 <akshatj2134@gmail.com>
2024-03-09 21:55:40 +0530

    user profile id complete

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,10 +3,13 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ProfilePage() {
 
     const router = useRouter();
+    const [data, setdata] = useState("");
+
     const logout = async () => {
         try {
             await axios.get('/api/users/logout');
@@ -17,12 +20,21 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const response = await axios.get('/api/users/me');
+        console.log(response.data);
+
+        setdata(response.data.data._id);
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <h2 className="bg-gray-500 rounded-md p-3">{data === "" ? "No Data" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
             <hr />
+            <button onClick={getUserDetails} className="bg-violet-500 hover:bg-violet-700 mt-4 text-white font-semibold py-2 px-4 rounded-md">Details</button>
             <button onClick={logout} className="bg-blue-500 hover:bg-blue-700 mt-4 text-white font-semibold py-2 px-4 rounded-md">Logout</button>
         </div>
     )
