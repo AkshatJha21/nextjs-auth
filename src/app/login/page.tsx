b8491f0d698cd163253671adf26e70fb4e861ae7
AkshatJha21 <akshatj2134@gmail.com>
2024-03-10 08:49:57 +0530

    login page ui done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
         try {
             setLoading(true);
             const response = await axios.post("/api/users/login", user);
-            console.log("Login success");
+            console.log("Login success", response.data);
             router.push("/profile")
         } catch (error: any) {
             console.log("Login failed", error.message);
@@ -38,15 +38,18 @@ export default function LoginPage() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>{loading ? "Processing" : "Login"}</h1>
-            <hr />
-            <label htmlFor="email">Email</label>
-            <input className="p-2 text-black border border-gray-300 rounded-sm mb-4 focus:outline-none focus:border-rose-500" type="text" id="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="email" />
-            <label htmlFor="password">Password</label>
-            <input className="p-2 text-black border border-gray-300 rounded-sm mb-4 focus:outline-none focus:border-rose-500" type="password" id="password" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} placeholder="password" />
-            <button onClick={onLogin} className="p-2 border border-gray-300 rounded-md mb-4">{buttonDisabled ? "No Login" : "Login"}</button>
-            <Link href="/signup">Visit signup page</Link>
-            <Link href="/resetpassword">Forgot password</Link>
+            <div className="bg-slate-800 p-6 rounded-md drop-shadow-md flex flex-col border border-slate-700 w-3/12">
+                <h1 className="mb-4 font-semibold text-lg">{loading ? "Processing" : "Login"}</h1>
+                <div className="flex flex-col">
+                    <label className="text-sm mb-1" htmlFor="email">Email</label>
+                    <input className="p-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-zinc-500 text-black" type="text" id="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} placeholder="email" />
+                    <label className="text-sm mb-1" htmlFor="password">Password</label>
+                    <input className="p-2 border border-gray-300 rounded-md mb-4 focus:outline-none focus:border-zinc-500 text-black" type="password" id="password" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} placeholder="password" />
+                </div>
+                <button onClick={onLogin} className="p-2 bg-gray-500 hover:bg-gray-600 transition-all rounded-md mb-4">{buttonDisabled ? "Enter Details" : "Login"}</button>
+                <Link className="flex justify-center mt-2 text-sm hover:underline" href="/signup">Visit signup page</Link>
+                <Link className="flex justify-center mt-2 text-sm hover:underline" href="/resetpassword">Forgot password</Link>
+            </div>
         </div>
     )
 }
